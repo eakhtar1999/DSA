c39cbc4425061e10f70714dd64a044d55521986e
Ehtesham Akhtar <54777668+eakhtar1999@users.noreply.github.com>
2026-05-10 07:25:29 -0500

    Time: 21 ms (5.05%) | Memory: 53.7 MB (15.39%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,33 +1,41 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if (nums1.length > nums2.length) {
+        if(nums1.length > nums2.length){
             return findMedianSortedArrays(nums2, nums1);
         }
+        int len1= nums1.length, len2 = nums2.length;
+        int left = 0, right= len1;
 
-        int len1 = nums1.length, len2 = nums2.length;
-        int left = 0, right = len1;
+         System.out.println(nums1.length+" "+ nums2.length);
+        while(left<=right){
+            int part1 = (left + right)/2;
+            int part2 = (len1 + len2 +1)/2 - part1;
 
-        while (left <= right) {
-            int part1 = (left + right) / 2;
-            int part2 = (len1 + len2 + 1) / 2 - part1;
+            int lmax1 = (part1==0) ? Integer.MIN_VALUE : nums1[part1-1];
+            int rmin1= (part1 == len1) ? Integer.MAX_VALUE : nums1[part1] ;
+            int lmax2 = (part2==0) ? Integer.MIN_VALUE : nums2[part2 -1 ];
+            int rmin2 = (part2 == len2) ? Integer.MAX_VALUE : nums2[part2];
 
-            int maxLeft1 = (part1 == 0) ? Integer.MIN_VALUE : nums1[part1 - 1];
-            int minRight1 = (part1 == len1) ? Integer.MAX_VALUE : nums1[part1];
-            int maxLeft2 = (part2 == 0) ? Integer.MIN_VALUE : nums2[part2 - 1];
-            int minRight2 = (part2 == len2) ? Integer.MAX_VALUE : nums2[part2];
+            System.out.println(lmax1+" "+ rmin1+" "+ lmax2+" "+ rmin2+" ");
 
-            if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
-                if ((len1 + len2) % 2 == 0) {
-                    return (Math.max(maxLeft1, maxLeft2) + Math.min(minRight1, minRight2)) / 2.0;
-                } else {
-                    return (double) Math.max(maxLeft1, maxLeft2);
+            if(lmax1 <= rmin2 && lmax2<= rmin1){
+                if((len1 + len2)%2 == 0){
+                    return (Math.max(lmax1, lmax2) + Math.min(rmin1, rmin2))/2.0;
                 }
-            } else if (maxLeft1 > minRight2) {
-                right = part1 - 1;
-            } else {
-                left = part1 + 1;
+                else{
+                    return (double) Math.max(lmax1, lmax2);
+                }
+            }else if(lmax1> rmin2){
+                right=part1-1;
+            }
+            else{
+                left=part1+1;
             }
         }
-        return 0.0;        
+        return 0.0;
     }
 }
+
+// [1 4  | 6 12 14]
+// [2 5|  29]. 
+// [ 1 2 4 5 | 6 12 14 29]. = 5.5
