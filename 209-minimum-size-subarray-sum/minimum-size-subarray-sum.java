d64819f103149de145847c7f144f40d2bb96e202
Ehtesham Akhtar <54777668+eakhtar1999@users.noreply.github.com>
2026-09-15 08:46:21 -0500

    Time: 2 ms (21.14%) | Memory: 69.5 MB (11.64%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -4,8 +4,6 @@ class Solution {
         int i=0,minCount=Integer.MAX_VALUE;
         for(int j=0; j < nums.length; j++){
             currSum+=nums[j];
-            System.out.println(currSum + " ");
-            if(currSum<target)continue;
             while(currSum>=target){
                 minCount = Math.min(minCount, j-i+1);
                 currSum-=nums[i];
